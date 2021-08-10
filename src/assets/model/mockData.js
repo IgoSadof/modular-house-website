e73fe63f10b318185eae8a5b5aa8ee4610ace2ea
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-08-10 22:47:55 +0300

    fix: try import  own glb model

--- src/assets/model/mockData.js
+++ src/assets/model/mockData.js
@@ -1,24 +1,8 @@
-// import * as THREE from 'three';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-// const scene = new THREE.Scene();
-import cubo from "../models3d/Cubo.glb"
-console.log(cubo)
-// const loader = new GLTFLoader();
-
-// loader.load( '../models3d/Cubo.glb', function ( gltf ) {
-
-// 	scene.add( gltf.scene );
-
-// }, undefined, function ( error ) {
-
-// 	console.error( error );
-
-// } );
 
 export const jsonMock = {
     linksGLB: [
-      "https://s3.amazonaws.com/modelviwer.dev/models-3d/Astronaut+(1).glb",
-      // loader,
+      "https://s3.amazonaws.com/modelviwer.dev/models-3d/Cubo.glb",
+      "https://storage.googleapis.com/modular-house/28_House.glb",
       "https://s3.amazonaws.com/modelviwer.dev/models-3d/bebe01.glb",
     ],
     linksUSDZ: [
