f3a503e9713e241ae7f2f0f908cf822d756d37da
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-08-11 00:20:08 +0300

    fix: try import  own glb model

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,59 +1,57 @@
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html" || stage === "develop-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /model-viewer/,
-              use: loaders.null(),
-            },
-            {
-              test: /\.(glb|gltf)$/,
-              use:[
-                {
-                  loader: 'file-loader',
-                  options: {
-                    outputPath: 'assets/models3d/',
-                  },
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//   if (stage === "build-html" || stage === "develop-html") {
+//     actions.setWebpackConfig({
+//       module: {
+//         rules: [
+//           {
+//             test: /model-viewer/,
+//             use: loaders.null(),
+//           },
+//         ],
+//       },
+//     });
+//   }
+// };
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /model-viewer/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  } else {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /\.(glb|gltf)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  outputPath: "assets/models3d/",
                 },
-              ],
-            },
-          ],
-        },
-      })
-    }
+              },
+            ],
+          },
+        ],
+      },
+      plugins: [
+        plugins.define({
+          __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
+        }),
+      ],
+    });
   }
-  // exports.onCreateWebpackConfig = ({
-  //   stage,
-  //   rules,
-  //   loaders,
-  //   plugins,
-  //   actions,
-  // }) => {
-  //   actions.setWebpackConfig({
-  //     module: {
-  //       rules: [
-  //         // {
-  //         //   test: /model-viewer/,
-  //         //   use: loaders.null(),
-  //         // },
-  //         {
-  //           test: /\.(glb|gltf)$/,
-  //           use:[
-  //             {
-  //               loader: 'file-loader',
-  //               options: {
-  //                 outputPath: 'assets/models3d/',
-  //               },
-  //             },
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //     plugins: [
-  //       plugins.define({
-  //         __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
-  //       }),
-  //     ],
-  //   })
-  // }
+};
