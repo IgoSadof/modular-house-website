ff5a129afd669572d4f3d3aca6982933e41f6bd2
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-08-11 09:28:18 +0300

    fix: try import  own glb model

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,50 +1,36 @@
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-//   if (stage === "build-html" || stage === "develop-html") {
-//     actions.setWebpackConfig({
-//       module: {
-//         rules: [
-//           {
-//             test: /model-viewer/,
-//             use: loaders.null(),
-//           },
-//         ],
-//       },
-//     });
-//   }
-// };
-exports.onCreateWebpackConfig = ({
-  stage,
-  rules,
-  loaders,
-  plugins,
-  actions,
-}) => {
-  if (stage === "build-html" || stage === "develop-html") {
+ exports.onCreateWebpackConfig = ({
+    stage,
+    rules,
+    loaders,
+    plugins,
+    actions,
+  }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /model-viewer/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
     actions.setWebpackConfig({
       module: {
         rules: [
-          {
-            test: /model-viewer/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  } else {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /\.(glb|gltf)$/,
-            use: [
-              {
-                loader: "file-loader",
-                options: {
-                  outputPath: "assets/models3d/",
+            {
+              test: /\.(glb|gltf)$/,
+              use:[
+                {
+                  loader: 'url-loader',
+                  options: {
+                    outputPath: 'assets/models3d/',
+                  },
                 },
-              },
-            ],
-          },
+              ],
+            },
         ],
       },
       plugins: [
@@ -52,6 +38,5 @@ exports.onCreateWebpackConfig = ({
           __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
         }),
       ],
-    });
+    })
   }
-};
