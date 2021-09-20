2d744138539f966712c1783c9f720078ce236ddc
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-09-20 10:09:56 +0300

    fix: switch off data base and useData

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,29 +11,29 @@ module.exports = {
       },
     },
     `gatsby-plugin-material-ui`,
-    {
-      resolve: `gatsby-source-mysql`,
-      options: {
-        connectionDetails: {
-          host: 'localhost',
-          user: 'root',
-          password: 'banderas021290',
-          database: 'atestmod_modx254'
-        },
-        queries: [
-          {
-            statement: 'SELECT * FROM modx_site_tmplvar_contentvalues',
-            idFieldName: 'id',
-            name: 'value',
-          },
-          {
-            statement: 'SELECT * FROM modx_site_content',
-            idFieldName: 'id',
-            name: 'parent',
-          },
-        ]
-      }
-    },
+    // {
+    //   resolve: `gatsby-source-mysql`,
+    //   options: {
+    //     connectionDetails: {
+    //       host: 'localhost',
+    //       user: 'root',
+    //       password: 'banderas021290',
+    //       database: 'atestmod_modx254'
+    //     },
+    //     queries: [
+    //       {
+    //         statement: 'SELECT * FROM modx_site_tmplvar_contentvalues',
+    //         idFieldName: 'id',
+    //         name: 'value',
+    //       },
+    //       {
+    //         statement: 'SELECT * FROM modx_site_content',
+    //         idFieldName: 'id',
+    //         name: 'parent',
+    //       },
+    //     ]
+    //   }
+    // },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
