ec8288b3643a7bbd381756d66ff0a3f6a7b6db40
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-02-02 09:56:11 +0300

    fix: getImg if path incorrect

--- src/utils/getImg.js
+++ src/utils/getImg.js
@@ -2,10 +2,11 @@
 import {getImage } from "gatsby-plugin-image";
 
 const getImg = (data,path) => {
+  console.log(data)
     return getImage(
       data.allFile.edges.find(
         (item) => item.node.relativePath === path.substr(7)
-      ).node
+      )?.node
     );
   };
   export default getImg
