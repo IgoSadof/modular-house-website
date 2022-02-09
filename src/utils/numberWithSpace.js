f92375ba7aa69b1c853c9577ef9f2e4263b914af
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-02-09 14:33:57 +0300

    fix: numberwithSpace

--- src/utils/numberWithSpace.js
+++ src/utils/numberWithSpace.js
@@ -1,8 +1,9 @@
 const numberWithSpace = (x) => {
-  return x
-    .toString()
-    .replace(/К|к|K|k/g, '000')
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  if(x){
+    return x.toString()
+      .replace(/К|к|K|k/g, '000')
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+  }
 };
 
 export default numberWithSpace;
