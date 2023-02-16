f4753328b9603f2974dd8b79ac9abc43d0d3aab7
IgoSadof <arevuare@mail.ru>
2023-02-16 12:00:49 +0300

    Игорь контаткы 10

--- src/components/ContactsElement.js
+++ src/components/ContactsElement.js
@@ -379,7 +379,7 @@ const ContactsElement = ({ header, data, dataContacts, lang }) => {
                 component='p'
               >
                 {/*dataContacts.phone*/}
-                +375 33 914 79 48
+                +7 921 252 8108
               </Typography>
               {/* {dataContacts.name ? ( */}
                 <Typography
@@ -399,7 +399,7 @@ const ContactsElement = ({ header, data, dataContacts, lang }) => {
                 component='p'
               >
                 {/*dataContacts.phone*/}
-                +375 33 914 79 48
+                +7 921 252 8108
               </Typography>
               {/* {dataContacts.name ? ( */}
                 <Typography
