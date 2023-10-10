62a18285d18a1cbef92e8a31ae31002db59fb7b8
IgoSadof <arevuare@mail.ru>
2023-10-10 12:44:55 +0300

    исправление высоты галереи интерьеров в проектах 4

--- src/components/sliders/FullScreenHouseSlider.js
+++ src/components/sliders/FullScreenHouseSlider.js
@@ -41,12 +41,12 @@ const useStyles = makeStyles((theme) => ({
       '-webkit-user-select': 'none',
       '-ms-user-select': 'none',
       [theme.breakpoints.down('md')]: {
-        //height: "520px",
+        height: (param) => (param.fullHeight ? '520px' : '100%'),
       },
     },
     [theme.breakpoints.down('md')]: {
       width:(param) => '100%',
-      height: (param) => (param.fullHeight ? '100%' : '520px'),
+      height: (param) => (param.fullHeight ? '100%' : '100%'),
       '@media (orientation: landscape)': {
         height: '100vh',
       },
